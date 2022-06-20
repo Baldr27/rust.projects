274f3a47360c0aeb4212142dc70275cb4cd11f58
Baldr27 <gjara.prg@gmail.com>
2022-06-19 20:02:56 -0400

    first

---
+++ BinaryTree/target/debug/deps/BinaryTree-34ff59b0dc8fd345.d
@@ -0,0 +1,5 @@
+/home/colonello/Documents/rust.projects/BinaryTree/target/debug/deps/BinaryTree-34ff59b0dc8fd345.rmeta: src/main.rs
+
+/home/colonello/Documents/rust.projects/BinaryTree/target/debug/deps/BinaryTree-34ff59b0dc8fd345.d: src/main.rs
+
+src/main.rs:
---
+++ BinaryTree/target/debug/deps/BinaryTree-86e513b24147f0a5.d
@@ -0,0 +1,5 @@
+/home/colonello/Documents/rust.projects/BinaryTree/target/debug/deps/BinaryTree-86e513b24147f0a5.rmeta: src/main.rs
+
+/home/colonello/Documents/rust.projects/BinaryTree/target/debug/deps/BinaryTree-86e513b24147f0a5.d: src/main.rs
+
+src/main.rs:
