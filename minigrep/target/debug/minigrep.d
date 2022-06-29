927820f7298d0e63c27693031a738c65276adb27
Baldr27 <107594029+Baldr27@users.noreply.github.com>
2022-06-29 01:01:46 -0400

    12-04

--- minigrep/target/debug/minigrep.d
+++ minigrep/target/debug/minigrep.d
@@ -1,1 +1,1 @@
-G:\rust.projects\minigrep\target\debug\minigrep.exe: G:\rust.projects\minigrep\src\main.rs
+G:\rust.projects\minigrep\target\debug\minigrep.exe: G:\rust.projects\minigrep\src\lib.rs G:\rust.projects\minigrep\src\main.rs
