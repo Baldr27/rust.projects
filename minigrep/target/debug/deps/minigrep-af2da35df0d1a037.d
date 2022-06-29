0a987206d80333f8fd71706e2e925bb8086f1519
Baldr27 <107594029+Baldr27@users.noreply.github.com>
2022-06-29 13:18:28 -0400

    minigrep pog

---
+++ minigrep/target/debug/deps/minigrep-af2da35df0d1a037.d
@@ -0,0 +1,5 @@
+g:\rust.projects\minigrep\target\debug\deps\minigrep-af2da35df0d1a037.rmeta: src\lib.rs
+
+g:\rust.projects\minigrep\target\debug\deps\minigrep-af2da35df0d1a037.d: src\lib.rs
+
+src\lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-ed9f5a34c56ef4ec.d
@@ -0,0 +1,5 @@
+g:\rust.projects\minigrep\target\debug\deps\minigrep-ed9f5a34c56ef4ec.rmeta: src\main.rs
+
+g:\rust.projects\minigrep\target\debug\deps\minigrep-ed9f5a34c56ef4ec.d: src\main.rs
+
+src\main.rs:
