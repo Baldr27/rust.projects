d97090affe1d88865f6edf88c2e40205951c61e6
Baldr27 <107594029+Baldr27@users.noreply.github.com>
2022-07-11 13:28:48 -0400

    empty

---
+++ Taller4/target/debug/deps/Taller4-1afc093c0b9a050d.d
@@ -0,0 +1,5 @@
+g:\rust.projects\Taller4\target\debug\deps\Taller4-1afc093c0b9a050d.rmeta: src\main.rs
+
+g:\rust.projects\Taller4\target\debug\deps\Taller4-1afc093c0b9a050d.d: src\main.rs
+
+src\main.rs:
