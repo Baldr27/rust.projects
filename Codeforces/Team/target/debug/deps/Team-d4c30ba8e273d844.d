3cc2db86ea5d3e73b27bb51463d72658a602a43b
Baldr27 <107594029+Baldr27@users.noreply.github.com>
2022-07-19 22:26:19 -0400

    empty

---
+++ Codeforces/Team/target/debug/deps/Team-d4c30ba8e273d844.d
@@ -0,0 +1,5 @@
+g:\rust.projects\Codeforces\Team\target\debug\deps\Team-d4c30ba8e273d844.rmeta: src\main.rs
+
+g:\rust.projects\Codeforces\Team\target\debug\deps\Team-d4c30ba8e273d844.d: src\main.rs
+
+src\main.rs:
